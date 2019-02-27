81894620ebcad72a57dd6787c2b4a68252e4bf54
djvu <djvu@inbox.ru>
2019-02-27 13:23:21 +0400

    first commit

---
+++ help/help.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.2
+#encoding: UTF-8
+
+import sys
+import inspect
+
+def getDigit(number, n):
+    """
+    Взять n-ю цифру из N-значного числа.
+        
+    Args:
+        s:       (Int) Число.
+        amount:  (Int) n-ая цифра.
+            
+    Returns:
+        (Int).
+    """
+    
+    return number // 10**n % 10
+    
+def LINE():
+    try:
+        raise Exception
+    except:
+        return sys.exc_info()[2].tb_frame.f_back.f_lineno
+
+def FILE():
+    """
+    Имя текущего файла.
+          
+    Returns:
+        String
+    """   
+    
+    return inspect.currentframe().f_code.co_filename
